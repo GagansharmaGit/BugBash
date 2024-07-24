1e3474cf5a0906b6cda48157773609ebd043718a
Gagan Sharma <Sharmagagan192@gmail.com>
2024-07-25 01:18:41 +0530

    cre questionCollection

---
+++ src/models/server/question.collection.ts
@@ -0,0 +1,57 @@
+import {IndexType, Permission} from "node-appwrite"
+
+import {db, questionCollection} from "../name";
+import { databases } from "./config";
+
+
+
+
+export default async function createQuestionCollections(){
+
+    // Creating collections
+    await databases.createCollection(db,questionCollection,questionCollection,
+        [
+            Permission.read("any"),
+            Permission.read("users"),
+            Permission.update("users"),
+            Permission.delete("users"),
+            Permission.create("users"),
+        ]
+    )
+
+    console.log("questionCollection is created");
+
+
+    //Creating Attributes 
+    await Promise.all([
+        databases.createStringAttribute(db,questionCollection,"title",100,true),
+        databases.createStringAttribute(db,questionCollection,"content",10000,true),
+        databases.createStringAttribute(db,questionCollection,"authorID",100,true),
+        databases.createStringAttribute(db,questionCollection,"title",100,true),
+        databases.createStringAttribute(db,questionCollection,"tags",100,true,undefined,true),
+        databases.createStringAttribute(db,questionCollection,"attachmentId",100,false),
+    ]);
+
+    console.log("Question Attributes is created");
+
+    //Creating Indexes
+    await Promise.all([
+        databases.createIndex(
+            db,
+            questionCollection,
+            "title",
+            IndexType.Fulltext,
+            ["title"],
+            ["asc"]
+        ),
+        databases.createIndex(
+            db,
+            questionCollection,
+            "content",
+            IndexType.Fulltext,
+            ["content"],
+            ["asc"]
+        ),
+    ])
+
+}
